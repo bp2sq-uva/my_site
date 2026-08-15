37faaf773ec1d93252c51b74d0740feab406ef6c
Bhasitha Dharmasena <bp2sq@virginia.edu>
2026-08-15 16:13:54 -0400

    debug-adding ML hit reconstruction project

--- src/app/lib/site.ts
+++ src/app/lib/site.ts
@@ -214,7 +214,7 @@ export const FEATURED_PROJECTS: Project[] = [
     ],
     images: [
       { src: "/projects/GNN-2.webp", alt: "GNNs" },
-      { src: "/projects/GNN_vid.dif", alt: "GNNs_vid" },
+      { src: "/projects/GNN_vid.gif", alt: "GNNs_vid" },
   ],
     description: `Graph Neural Networks (GNNs) have emerged as powerful tools for modeling complex relationships in data, making them well-suited for particle tracking in high-rate nuclear physics detectors. This project focused on developing and implementing GNN architectures to enhance tracking accuracy and efficiency. By representing detector hit data as nodes and their spatial relationships as edges, the GNN models were able to learn intricate patterns in the data. This work has significant implications for advancing reconstruction techniques in nuclear and particle physics experiments.`,
   },
